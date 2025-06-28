4a480953c3320618d9e890549281c548e147806f
CristhianLozanoT <cristhian.lozano.t@uniautonoma.edu.co>
2025-06-28 10:36:28 -0500

    Migración Nominas

--- database/migrations/2025_06_28_150523_create_nominas_table.php
+++ database/migrations/2025_06_28_150523_create_nominas_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('nominas', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->integer('dias_laborados');
+            $table->integer('horas_extras')->nullable()->default(0);
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
         });
     }
 
