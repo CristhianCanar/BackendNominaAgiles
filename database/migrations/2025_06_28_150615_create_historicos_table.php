6829526f8a4709f82f0125ecc9fc710b16c3480c
brayancuella <brayan.cuellar.anturi@gmail.com>
2025-06-28 10:31:08 -0500

    migracion historicos

--- database/migrations/2025_06_28_150615_create_historicos_table.php
+++ database/migrations/2025_06_28_150615_create_historicos_table.php
@@ -13,7 +13,25 @@ return new class extends Migration
     {
         Schema::create('historicos', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('nomina_id');
+            $table->double('prima_servicio');
+            $table->double('servicios_cesantias');
+            $table->double('interes_cesantias');
+            $table->double('vacaciones');
+            $table->double('salario_proporcional');
+            $table->double('auxilio_transporte');
+            $table->double('horas_extras');
+            $table->double('total_devengado');
+            $table->double('salud');
+            $table->double('pension');
+            $table->double('otros_descuentos');
+            $table->double('total_deducciones');
+            $table->double('salario_neto');
             $table->timestamps();
+
+            $table ->foreign('user_id')->references('id')->on('users');
+            $table->foreign('nomina_id')->references('id')->on('nominas');
         });
     }
 
